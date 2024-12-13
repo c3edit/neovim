887c0ae26c9f0dadc45be1af2602f831355873af
Adam Zhang <898544@lcps.org>
2024-12-12 19:43:20 -0500

    feat: add command to start backend

--- lua/c3edit/init.lua
+++ lua/c3edit/init.lua
@@ -1,7 +1,29 @@
 local M = {}
+local backend_process = nil
 
 function M.greet()
     print("Hello from your plugin!")
+end
+
+function M.start_backend()
+    backend_process = vim.fn.jobstart({"/home/ad/git/c3edit-backend/target/debug/c3edit"}, {
+        on_stdout = function(_, data)
+            if data then
+                print(table.concat(data, "\n"))
+            end
+        end,
+        on_stderr = function(_, data)
+            if data then
+                print("Error: " .. table.concat(data, "\n"))
+            end
+        end,
+        on_exit = function(_, code)
+            print("Command exited with code " .. code)
+        end,
+        stdin = "pipe",
+    })
+
+    print("Backend started")
 end
 
 return M
--- plugin/c3edit.lua
+++ plugin/c3edit.lua
@@ -1,3 +1,7 @@
 vim.api.nvim_create_user_command('Greet', function()
     require('c3edit').greet()
 end, {})
+
+vim.api.nvim_create_user_command('StartBackend', function()
+    require('c3edit').start_backend()
+end, {})
